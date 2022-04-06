df97c60801b9f441857ae220cc42caf16ed5354d
Jakub Müller <jakub.muller.so@gmail.com>
2022-04-06 16:48:41 +0200

    added mongo doc count

--- integration_test.py
+++ integration_test.py
@@ -1,3 +1,12 @@
-print("hihi")
-
+from pymongo import MongoClient
+  
+Client = MongoClient()
+myclient = MongoClient('localhost', 27017)
+  
+my_database = myclient["ams"]  
+my_collection = my_database["articles"] 
+  
+# number of documents in the collection
+mydoc = my_collection.find().count()
+print("The number of documents in collection : ", mydoc) 
     
