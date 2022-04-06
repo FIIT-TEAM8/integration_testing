bb5dbec02b5240ced986134313f5da42cc67d737
Jakub Müller <jakub.muller.so@gmail.com>
2022-04-06 17:15:48 +0200

    credentials update

--- integration_test.py
+++ integration_test.py
@@ -1,12 +1,21 @@
 from pymongo import MongoClient
+
+local_mongo_uri = 'mongodb://mongodb0.example.com:27017'
+mongo_username = 'test'
+mongo_password = 'test'
+
+# connect to local MongoDB container
+my_client = MongoClient(
+    host = local_mongo_uri,
+    serverSelectionTimeoutMS = 3000,
+    username = mongo_username,
+    password = mongo_password
+)
   
-Client = MongoClient()
-myclient = MongoClient('localhost', 27017)
-  
-my_database = myclient["ams"]  
-my_collection = my_database["articles"] 
+my_database = my_client["ams"]  
+my_collection = my_database["articles"]
   
 # number of documents in the collection
-mydoc = my_collection.find().count()
+mydoc = my_collection.find().count_documents()
 print("The number of documents in collection : ", mydoc) 
     
