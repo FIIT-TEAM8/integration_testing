df3128c629d3dd0e46c03a8dbe93ad6e0c188e62
Jakub Müller <jakub.muller.so@gmail.com>
2022-04-06 17:56:54 +0200

    Update integration_test.py

--- integration_test.py
+++ integration_test.py
@@ -2,18 +2,7 @@ from threading import local
 from pymongo import MongoClient
 import requests
 
-#local_mongo_uri = 'mongodb://localhost:27017/ams'
 local_mongo_uri = 'mongodb://test:test@localhost:27017/'
-mongo_username = 'test'
-mongo_password = 'test'
-
-# connect to local MongoDB container
-# my_client = MongoClient(
-#     host = local_mongo_uri,
-#     serverSelectionTimeoutMS = 3000,
-#     username = mongo_username,
-#     password = mongo_password
-# )
 
 my_client = MongoClient(local_mongo_uri)
   
@@ -26,7 +15,7 @@ print("The number of documents in collection : ", mydoc)
 
 param = (('v', ''),) # '-v' is for --verbose
 
-resp = requests.get('http://localhost:9200/_cat/indices', params=param)
+resp = requests.get('http://es01:9200/_cat/indices', params=param)
 print ('\nHTTP code:', resp.status_code, '-- response:', resp, '\n')
 print ('dir:', dir(resp), '\n')
 print ('response text:', resp.text)
